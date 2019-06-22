feda736e96395aa289fb9c79640be6ce0aece240
Markellus <gmx@marcel-bulla.de>
2019-06-22 12:58:15 +0200

    Arithmetic Mean

---
+++ Statistics/ArithmeticMean.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+
+namespace De.Markellus.Maths.Statistics
+{
+    public static class ArithmeticMean
+    {
+        /// <summary>
+        /// Berechnet das arithmetische Mittel der Werte in der übergebenen Liste.
+        /// </summary>
+        /// <param name="listValues">Eine Liste mit Werten, dessen arithmetisches Mittel berechnet werden soll.</param>
+        /// <returns>Das arithmetische Mittel der übergebenen Werte.</returns>
+        public static double Calculate(double[] listValues)
+        {
+            return listValues.Sum() / listValues.Count();
+        }
+
+        /// <summary>
+        /// Berechnet das arithmetische Mittel der Werte in der übergebenen Liste.
+        /// </summary>
+        /// <param name="listValues">Eine Liste mit Werten, dessen arithmetisches Mittel berechnet werden soll.</param>
+        /// <returns>Das arithmetische Mittel der übergebenen Werte.</returns>
+        public static float Calculate(float[] listValues)
+        {
+            return listValues.Sum() / listValues.Count();
+        }
+
+        /// <summary>
+        /// Berechnet das arithmetische Mittel der Werte in der String-Liste. Die Strings werden als Gleitkommazahlen interpretiert.
+        /// </summary>
+        /// <param name="arrValues">Eine Liste mit String-Werten, dessen arithmetisches Mittel berechnet werden soll.</param>
+        /// <returns>Das arithmetische Mittel der übergebenen Werte.</returns>
+        public static double Calculate(string[] arrValues)
+        {
+            return Calculate(InputParser.ParseDoubleList(arrValues));
+        }
+
+        /// <summary>
+        /// Berechnet das arithmetische Mittel der Werte in der String-Liste. Die Strings werden als Gleitkommazahlen interpretiert.
+        /// </summary>
+        /// <param name="strValues">Eine Liste mit String-Werten, dessen arithmetisches Mittel berechnet werden soll.</param>
+        /// <param name="cDivider">Das Zeichen, mit dem einzelne Werte voneinander getrennt sind.</param>
+        /// <returns>Das arithmetische Mittel der übergebenen Werte.</returns>
+        public static double Calculate(string strValues, char cDivider = ';')
+        {
+            return Calculate(InputParser.ParseDoubleList(strValues, cDivider));
+        }
+    }
+}
