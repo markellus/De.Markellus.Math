39526f5eacf6aca62e62c37bff92593b5579e379
Markellus <gmx@marcel-bulla.de>
2019-07-22 13:43:17 +0200

    Moved the real datatype outside the internals namespace, as it is not internal.

--- src/Arithmetic/Real.cs
+++ src/Arithmetic/Real.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
-namespace De.Markellus.Maths.Internals.Arithmetic
+namespace De.Markellus.Maths.Arithmetic
 {
     /// <summary>
     /// Diese Klasse bildet eine Zahl aus der Menge der reellen Zahlen ab.
